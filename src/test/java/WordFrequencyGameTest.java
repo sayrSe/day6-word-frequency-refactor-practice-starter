56b0c2de9e03f82f30307ee3cd95b509988bd01a
Jose Angelo Arboleda <joseangelo.arboleda@gmail.com>
2023-08-21 22:06:35 +0800

    test: remove never thrown exception

--- src/test/java/WordFrequencyGameTest.java
+++ src/test/java/WordFrequencyGameTest.java
@@ -6,7 +6,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 public class WordFrequencyGameTest {
 
     @Test
-    public void should_get_the_1_when_input_the() throws Exception {
+    public void should_get_the_1_when_input_the() {
         //Given
         String inputStr = "the";
         String expectResult = "the 1";
@@ -14,7 +14,7 @@ public class WordFrequencyGameTest {
     }
 
     @Test
-    public void should_process_two_words() throws Exception {
+    public void should_process_two_words() {
         //Given
         String inputStr = "the is";
         String expectResult = "the 1\nis 1";
@@ -22,7 +22,7 @@ public class WordFrequencyGameTest {
     }
 
     @Test
-    public void should_process_two_words_with_special_spaces() throws Exception {
+    public void should_process_two_words_with_special_spaces() {
         //Given
         String inputStr = "the      is";
         String expectResult = "the 1\nis 1";
@@ -30,7 +30,7 @@ public class WordFrequencyGameTest {
     }
 
     @Test
-    public void should_process_two_words_with_special_enter() throws Exception {
+    public void should_process_two_words_with_special_enter() {
         //Given
         String inputStr = "the   \n   is";
         String expectResult = "the 1\nis 1";
@@ -38,7 +38,7 @@ public class WordFrequencyGameTest {
     }
 
     @Test
-    public void should_process_two_same_words_with_sorted() throws Exception {
+    public void should_process_two_same_words_with_sorted() {
         //Given
         String inputStr = "the the is";
         String expectResult = "the 2\nis 1";
@@ -46,7 +46,7 @@ public class WordFrequencyGameTest {
     }
 
     @Test
-    public void should_process_sorted_with_count_descending() throws Exception {
+    public void should_process_sorted_with_count_descending() {
         //Given
         String inputStr = "the is is";
         String expectResult = "is 2\nthe 1";
