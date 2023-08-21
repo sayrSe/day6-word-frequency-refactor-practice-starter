38ea8b107ee4207b228dbfbb5663e0eda27bb41b
Jose Angelo Arboleda <joseangelo.arboleda@gmail.com>
2023-08-21 22:00:37 +0800

    refactor: specify imports

--- src/main/java/word/frequency/WordFrequencyGame.java
+++ src/main/java/word/frequency/WordFrequencyGame.java
@@ -1,6 +1,8 @@
 package word.frequency;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class WordFrequencyGame {
